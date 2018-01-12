1b8f57924c42520760dd9aa89a2585f1d62d0f35
Ryan Roat <ryanroat@gmail.com>
2018-01-12 12:52:38 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,5 +1,7 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 
 function addElementToBeginningOfArray(theArray, newEl) {
-  return 
+  return [newEl, theArray];
 }
+
+function
